6edd774a61ffc608da209f65cf97ed8199877373
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-03-24 21:02:59 +0200

    add User model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female')
+        ('O', 'Other'),
+    )
+    customer_id = models.CharField(max_length=50, unique=True)
+    gender = models.CharField(max_length=20, choices=GENDER)
+    age = models.IntegerField()
+
+
+class Account(models.Model):
+    pass

f0d4e50e3ee505a3c407f0913aa9fb27a15cc2f6
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-03-24 21:25:22 +0200

    add account number generation signal

--- api/signals.py
+++ api/signals.py
@@ -1,13 +1,24 @@
 from .models import User, Account
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-import uuid
+import uuid, string, random
 
 
 @receiver(pre_save, sender=User)
 def generate_customer_id(sender, instance, **kwargs):
   '''
-  Generate random UUID code for user identification.
+  Generate UUID code
   '''
   if not instance.customer_id:
       instance.customer_id = str(uuid.uuid4())[:12].replace('-', '')
+
+
+@receiver(pre_save, sender=Account)
+def generate_account_number(sender, instance, **kwargs):
+    '''
+    Generate random alphanumeric code
+    '''
+    if not instance.number:
+       letters = ''.join(random.choices(string.ascii_letters, k=4)).upper()
+       digits = ''.join(random.choices(string.digits, k=10))
+       instance.number = f'{ letters }_{ digits }'

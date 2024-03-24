c7334e53b7c531b6a942b92fc943eca93c4d1c0e
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-03-24 21:46:23 +0200

    Update: add account serializer & user serializer create method

---
+++ api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import User, Account, Transaction
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {
+            'password': { 'write_only': True },
+        }
+      
+    def create(self, valiated_data):
+        user = User(**valiated_data)
+        user.set_password(valiated_data['password'])
+        user.save()
+        return user
+
+
+class AccountSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'

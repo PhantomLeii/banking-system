bc91635dbea95b1eb7e8766f50bc2a85727c8a65
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-03-24 21:15:14 +0200

    add Account model

--- api/models.py
+++ api/models.py
@@ -14,4 +14,19 @@ class User(AbstractUser):
 
 
 class Account(models.Model):
-    pass
+    TYPES = (
+        ('current', 'Current'),
+        ('savings', 'Savings'),
+    )
+
+    STATUS = (
+        ('active', 'Active'),
+        ('frozen', 'Frozen'),
+        ('disabled', 'Disabled'),
+    )
+    number = models.CharField(max_length=50, unique=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=50, choices=TYPES)
+    status = models.CharField(max_length=50, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)

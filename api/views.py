b4d7bf9f49266a739c9ceaccdb26ea0f05c8a4f0
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-03-25 09:41:12 +0200

    Update: add jwt authentication utility

--- api/views.py
+++ api/views.py
@@ -1,13 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework_jwt.views import obtain_jwt_token
 from .models import User, Account, Transaction
+
+ObtainJWTToken = obtain_jwt_token
 
 
 class UserAPView(APIView):
     def get_user(self, request) -> User:
         '''
         Read user credentials from request
+        authorization header
         '''
         pass    
     

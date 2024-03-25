8b7ad414c13df2ba2be6653c23bd2d11c719cc96
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-03-25 11:38:34 +0200

    Update: add user information retrieval functionality

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.shortcuts import get_object_or_404
+
 from .models import User, Account, Transaction
 from .serializers import UserSerializer
 
@@ -16,6 +18,14 @@ class UserAPIView(APIView):
         serializer.save()
         
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def get(self, request):
+        '''
+        View all user profile data
+        '''
+        users = User.objects.get(id=1)
+        serializer = UserSerializer(users)
+        return Response(serializer.data, status.HTTP_200_OK)
 
 
 class AccountAPIView(APIView):

242631e951700cb467403977c9adde010be01c1c
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-03-25 21:02:01 +0200

    Update: add account listing functionality

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserAPIView
+from .views import UserAPIView, AccountAPIView
 
 
 urlpatterns = [
     path('user/', UserAPIView.as_view(), name='user'),
+    path('account/', AccountAPIView.as_view(), name='account'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,20 +3,21 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import User, Account, Transaction
-from .serializers import UserSerializer
+from .serializers import UserSerializer, AccountSerializer
+
+
+def get_user_object(request):
+    """Isolate user object"""
+    try:
+        user = User.objects.get(id=2)
+    except User.DoesNotExist:
+        return Response({
+            'detail': 'User not found'
+        }, status.HTTP_404_NOT_FOUND)
+    return user
 
 
 class UserAPIView(APIView):
-    def get_user_object(self, request):
-        try:
-            user = User.objects.get(id=2)
-        except User.DoesNotExist:
-            return Response({
-                'detail': 'User not found'
-            }, status.HTTP_404_NOT_FOUND)
-
-        return user
-
     def post(request):
         """Register new user"""
         serializer = UserSerializer(data=request.data)
@@ -27,13 +28,13 @@ class UserAPIView(APIView):
 
     def get(self, request):
         """View all user profile data"""
-        instance = self.get_user_object(request)
+        instance = get_user_object(request)
         serializer = UserSerializer(instance)
         return Response(serializer.data, status.HTTP_200_OK)
 
     def patch(self, request):
         """Partially update user profile data"""
-        instance = self.get_user_object(request)
+        instance = get_user_object(request)
 
         serializer = UserSerializer(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
@@ -42,27 +43,30 @@ class UserAPIView(APIView):
 
     def delete(self, request):
         """Delete current user profile"""
-        instance = self.get_user_object(request)
+        instance = get_user_object(request)
         instance.delete()
         return Response({
             'detail': 'Deleted successfully'
         }, status.HTTP_204_NO_CONTENT)
 
 
+def get_user_accounts(request):
+    """Isolate accounts owned by current user"""
+    user = get_user_object(request)
+    accounts = Account.objects.filter(user_id=user)
+    if accounts:
+        return accounts
+    return Response({
+        'detail': 'No accounts'
+    }, status.HTTP_204_NO_CONTENT)
+
+
 class AccountAPIView(APIView):
-
-    @staticmethod
-    def user_object(request):
-        try:
-            user = User.objects.get(id=2)
-        except User.DoesNotExist:
-            return Response({
-                'detail': "user not found"
-            }, status.HTTP_404_NOT_FOUND)
-
-        return user
-
-
+    def get(self, request):
+        """View owned bank account data"""
+        accounts = get_user_accounts(request)
+        serializer = AccountSerializer(accounts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class TransactionAPIView(APIView):
     pass

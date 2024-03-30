017a31338f0efbefc8a72651fbf4f3ec8d840d59
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-03-30 13:53:41 +0200

    Update: add auth for token generation

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,4 @@ from .views import UserAPIView, AccountAPIView, CreateUserView
 
 urlpatterns = [
     path('user/', UserAPIView.as_view(), name='user'),
-    path('signup/', CreateUserView.as_view(), name='user-signup'),
-    path('account/', AccountAPIView.as_view(), name='account'),
 ]

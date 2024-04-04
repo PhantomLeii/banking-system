3d81e2ab49072381a6276c4ab169fed41eeb21c7
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-04-04 14:06:03 +0200

    Update: add homepage view & navbar

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from .models import Account, Transaction
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'routes/index.html'

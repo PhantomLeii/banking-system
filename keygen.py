a34db8b1a26cd15d7b1c5ecc0dd840589cc0b606
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-05-10 00:42:39 +0200

    add key generation script

--- keygen.py
+++ keygen.py
@@ -1,39 +1,22 @@
-import secrets, string, sys
-
-FLAGS = ('-r', '--refresh')
-HELP = (
-    """
-    Usage:
-      python keygen.py           : Generate & print secret key (Not assigned to project)
-      python keygen.py -r        : Generate & reset to new secret-key (Assign to project & not printed)
-      python keygen.py --refresh : Generate & reset to new secret-key(Assigned to project & not printed)
-    """
-).strip()
-
+import secrets, string, sys, os 
 
 def main():
-    args = sys.argv[1:]
-    flag = args[0]
+    dot_env_path = os.path.join('config', '.env')
 
-    if len(args) > 1:
-        print(HELP)
-        sys.exit()
+    write_secret(dot_env_path, generate_secret(50))
     
-    if flag.strip().lower() not in FLAGS:
-        print(HELP)
-        sys.exit()
-
-    secret = generate_secret()
-    print(secret)
-    
-
-
+    return None
 
 def generate_secret(length=50):
     """Generate secret key for django project"""
     characters = string.ascii_letters + string.digits + string.punctuation
     secret = ''.join(secrets.choice(characters) for _ in range(length))
     return secret
+
+def write_secret(path, secret):
+    """Write secret to the file specified in path"""
+    with open(path, 'w') as env:
+        env.write(f'SECRET="{secret}"')
 
 if __name__ == '__main__':
     main()

bc1540525cec13a0592e5e1327ce2dc2f6e61a36
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-06-11 09:30:00 +0200

    delete all files

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-from core.models import Account, Transaction
-
-User = get_user_model()
-
-admin.site.register(User)
-admin.site.register(Account)
-admin.site.register(Transaction)

9f8f60568a542872c608ca5e707034b20d84b5af
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-06-29 08:52:39 +0200

    Update: add delete user view

--- server/users/views.py
+++ server/users/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    UpdateAPIView,
+    RetrieveAPIView,
+    DestroyAPIView,
+)
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -26,3 +31,20 @@ class UpdateUserView(UpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+
+class DeleteUserView(DestroyAPIView):
+    def get_object(self):
+        return self.request.user
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)

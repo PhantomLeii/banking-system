13ef9654cdb9695d45aa8bba83193d69dddc5205
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-06-29 09:45:13 +0200

    Update: add Account model

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Account(models.Model):
+    ACCOUNT_TYPE = (
+        ("Savings", "Savings"),
+        ("Current", "Current"),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=50, choices=ACCOUNT_TYPE)
+    account_number = models.CharField(max_length=50, unique=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.account_number
--- server/core/settings.py
+++ server/core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "users",
+    "accounts",
 ]
 
 MIDDLEWARE = [
